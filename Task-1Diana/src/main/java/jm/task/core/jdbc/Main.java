fb594c543d29085bf0dbf06c9e752f768c85e4fe
DianaN64-alt <diananursubina64@gmail.com>
2024-07-17 08:04:56 +0100

    Add files via upload

---
+++ Task-1Diana/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,30 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main {
+    private static UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+
+    public static void main(String[] args) {
+//         реализуйте алгоритм здесь
+        userDaoJDBC.dropUsersTable();
+
+        userDaoJDBC.createUsersTable();
+
+
+        userDaoJDBC.saveUser("Alexander", "Pushkin", (byte) 37);
+        userDaoJDBC.saveUser("Nicolai", "Gogol", (byte) 42);
+        userDaoJDBC.saveUser("Lev", "Tolstoi", (byte) 82);
+        userDaoJDBC.saveUser("Marina", "Tsvetaeva", (byte) 48);
+
+        userDaoJDBC.getAllUsers().forEach(System.out::println);
+
+        userDaoJDBC.cleanUsersTable();
+
+        userDaoJDBC.getAllUsers().forEach(System.out::println);
+
+        userDaoJDBC.dropUsersTable();
+
+    }
+}
